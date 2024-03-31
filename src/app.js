33b7a4cd0a57a96cfb84d46dc3e5305773398591
Parth Ahuja <parthahuja.in@gmail.com>
2024-03-31 22:37:46 +0530

    changes in env file

--- src/app.js
+++ src/app.js
@@ -2,8 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const router = require("./routers/index");
 
-const port = process.env.PORT || 3000;
-
+const port = process.env.PORT;
 const app = express();
 
 app.use(express.json());
@@ -11,5 +10,5 @@ app.use(express.json());
 app.use("/", router);
 
 app.listen(port, () => {
-  console.log("server is running on ", port);
+  console.log("server is running on port", port);
 });
